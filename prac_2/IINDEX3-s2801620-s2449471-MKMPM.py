a7f2a9a632381d5df6efe80bd4d9f162c65e3f0c
peshmerge <pesho.morad@gmail.com>
2021-12-01 14:45:54 +0100

    added regex to remove punctuation from words

--- prac_2/IINDEX3-s2801620-s2449471-MKMPM.py
+++ prac_2/IINDEX3-s2801620-s2449471-MKMPM.py
@@ -4,9 +4,9 @@ Manish Mishra s2801620
 Peshmerge Morad s2449471
 
 Our time is :
-real	0m8.440s
-user	0m13.460s
-sys	0m1.256s
+real	0m7.825s
+user	0m13.693s
+sys	0m1.030s
 
 This computes the inverted index for a document base in  /data/doina/Gutenberg-EBooks.
 This program is written in Python3
@@ -16,6 +16,7 @@ To execute on a machine:
 """
 
 from pyspark import SparkContext
+import re
 
 sc = SparkContext(appName="IINDEX-MKMPM")
 sc.setLogLevel("ERROR")
@@ -23,14 +24,14 @@ sc.setLogLevel("ERROR")
 rdd = sc.wholeTextFiles("/data/doina/Gutenberg-EBooks")
 
 # using flatMapValues to map file, contents of RDD to file, set(unique words)
-words_iindex = rdd.flatMapValues(lambda contents: set(contents.lower().split()))
+# convert all letters to lower case and remove punctuations using regex
+words_iindex = rdd.flatMapValues(lambda contents: set((re.sub(r'[^\w\s]', '', contents)).lower().split()))
 # swapping (file, word) to (word, set(file)) because of inverse index
-words_iindex =  words_iindex.map(lambda item: (item[1],{item[0]}))
+words_iindex = words_iindex.map(lambda item: (item[1], {item[0]}))
 # reducing by key to get desired result
-words_iindex =  words_iindex.reduceByKey(lambda a,b:a|b)
+words_iindex = words_iindex.reduceByKey(lambda a, b: a | b)
 
 # filter and process all the above steps
 filtered_words_iindex = words_iindex.filter(lambda item: len(item[1]) > 12).collect()
 
-for (word, documents_list) in filtered_words_iindex:
-    print(word)
+[print(word, end=' ') for (word, _) in filtered_words_iindex]
