7890302e2020ab92daace4d629aabfdc6292f0ac
peshmerge <pesho.morad@gmail.com>
2021-12-01 17:37:25 +0100

    knn first attempt

--- prac_2/KNN3-s2801620-s2449471-MKMPM.py
+++ prac_2/KNN3-s2801620-s2449471-MKMPM.py
@@ -5,7 +5,9 @@ Peshmerge Morad s2449471
 
 Our time is :
 
-This computes the inverted index for a document base in  /data/doina/Gutenberg-EBooks.
+
+
+This computes KNN for any given point based on the data in the csv file located in /data/doina/xyvalue.csv.
 This program is written in Python3
 
 To execute on a machine:
@@ -17,7 +19,17 @@ from pyspark import SparkContext
 # fetch spark context and set log level to ERROR only
 sc = SparkContext(appName="KNN-MKMPM")
 sc.setLogLevel("ERROR")
-
-# read all input files
 rdd = sc.textFile("/data/doina/xyvalue.csv")
 
+def calc_dist(p1,p2):
+    return ((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)**(1/2)
+input = (50, 50)
+k = 100
+
+values = rdd \
+    .map(lambda line: line.split(",")) \
+    .map(lambda line: [float(point) for point in line]) \
+    .map(lambda point: (point[0], point[1], point[2], round(calc_dist(input, (point[0], point[1]))),4))\
+    .map(lambda point: (point[3], point[2])).sortByKey()
+value = sum(value[1] for value in values.take(k)) /k
+print(value)
