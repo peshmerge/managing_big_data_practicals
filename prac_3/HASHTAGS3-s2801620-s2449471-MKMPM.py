bbe47f9e8232d6aafaabe385eff98d7662309dfb
zedd-08 <manish.96@live.com>
2021-12-06 13:43:09 +0100

    Added comments

--- prac_3/HASHTAGS3-s2801620-s2449471-MKMPM.py
+++ prac_3/HASHTAGS3-s2801620-s2449471-MKMPM.py
@@ -4,9 +4,9 @@ Manish Mishra s2801620
 Peshmerge Morad s2449471
 
 Our time is :
-real    0m15.776s
-user    1m28.079s
-sys     0m3.443s
+real    0m15.987s
+user    1m26.617s
+sys     0m3.496s
 
 This computes the top 20 hashtags for first 10 min from tweet database in /data/doina/Twitter-Archive.org/2020-01/01/00/.
 This program is written in Python3
@@ -28,5 +28,8 @@ tweets_loc = '/data/doina/Twitter-Archive.org/2020-01/01/00/0*.json.bz2'
 spark = SparkSession.builder.getOrCreate()
 df = spark.read.json(tweets_loc)
 
+# collect all text from the 'entities.hashtags' column using explode() and put new column alias as 'hashtags'
 hashtags = df.select(func.explode(df.entities.hashtags.text).alias('hashtags'))
+
+# count hashtag groups and show the count
 hashtags.groupBy('hashtags').count().orderBy('count', ascending=False).show()
